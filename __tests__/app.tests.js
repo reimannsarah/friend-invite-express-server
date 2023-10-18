615248073eedc1f3e5b38d5302e85b68092da4f5
Sarah Reimann <reimannsarah45@gmail.com>
2023-10-18 14:57:28 -0700

    passing test

--- __tests__/app.tests.js
+++ __tests__/app.tests.js
@@ -9,3 +9,16 @@ test('GET /friends should return an array of friends', async () => {
   expect(Array.isArray(friends)).toBe(true);
 });
 
+// POST /friends/invite
+test('POST /friends/invite should return a success message', async () => {
+  const response = await fetch(`${serverUrl}/friends/invite`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(['john.doe@example.com', 'jane.doe@example.com']),
+  });
+
+  const data = await response.json();
+  expect(data).toHaveProperty('message');
+});
