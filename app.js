234f43889381498ec6c982c40dad873e13495eef
Sarah Reimann <reimannsarah45@gmail.com>
2023-10-18 10:56:36 -0700

    change index to app

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 // app.use(cors);
 
-app.listen(port, () => {
+app.listen(port || 3000, () => {
   console.log(`Server is running on port ${port}`)
 });
 
